a61db75d249b2122dcaca5def9aa9b0926812b26
Franklin <franklinbmw@msn.com>
2018-09-06 19:24:40 -0700

    remove useless comment

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos'
 
-import img from './img/color-pattern.jpeg'
-
-// const mainBg = 'url("./img/color-pattern.jpeg")';
-
 class App extends Component {
-  // componentDidMount(){
-  //   document.body.style= {mainBg};
-  // }
   render() {
     return (
       <div className="App" id="circular">
