f4b6e252b3cf9891349e4cfe24cc54e98629cbb7
Franklin <franklinbmw@msn.com>
2018-09-06 19:14:25 -0700

    add some awesome background pattern using css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Todos from './components/Todos'
 
+import img from './img/color-pattern.jpeg'
+
+// const mainBg = 'url("./img/color-pattern.jpeg")';
+
 class App extends Component {
+  // componentDidMount(){
+  //   document.body.style= {mainBg};
+  // }
   render() {
     return (
-      <div className="App">
+      <div className="App" id="circular">
        <Todos />
       </div>
     );
